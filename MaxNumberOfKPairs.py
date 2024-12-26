5126ae50d3df5d5dc4eac63a3ac7ece70bb44af9
Hemant Kadam <140627846+hemantkadam0656@users.noreply.github.com>
2024-12-27 00:49:49 +0530

    new code for k pairs

--- MaxNumberOfKPairs.py
+++ MaxNumberOfKPairs.py
@@ -1,14 +1,42 @@
 def maxNumberOfKPairs(arr, k,):
-    n = len(arr)
-    i = 0
     count = 0
+    n = len(arr)
+    i = 0 
+    j = n
+
+    if len(arr) < 2 and arr[0] == k:
+        return 1
     
-    if n < 2:
-        return 0
-    
-  
+    if len(arr) < 2 and arr[0] != k:
+        return 0 
+
+    first_num = sec_num = 0
+    while i < j: 
+
+        if arr[i] <= k:
+            first_num = arr[i]
+        i += 1 
+
+        if arr[j-1] <= k:
+            sec_num = arr[j-1]
+        j -= 1
+       
+        if first_num + sec_num == k :
+            count +=1
+
+
+
+    return count
+
+
+
+
 
 if __name__ == '__main__':
     arr = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
     k = 2
     obj = maxNumberOfKPairs(arr, k)
+    print(obj)
+
+
+ 
